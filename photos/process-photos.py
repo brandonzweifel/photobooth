dc5aaacecbb2fc2fb7404c724fefa97e13ac1890
rmulligan <rmulligan@fatwallet.com>
2015-07-24 14:55:17 -0500

    Changing things around

--- photos/process-photos.py
+++ photos/process-photos.py
@@ -9,19 +9,16 @@ os.chdir(os.path.dirname(os.path.realpath(__file__)))
 template = Image.open("template-base.png")
 
 # Create the composite
-template.paste(Image.open("image1.jpg").resize((713, 536)), (356, 60))
-template.paste(Image.open("image2.jpg").resize((713, 536)), (1084, 60))
-template.paste(Image.open("image3.jpg").resize((713, 536)), (356, 608))
-template.paste(Image.open("image4.jpg").resize((713, 536)), (1084, 608))
-
-overlay = Image.open("template-overlay.png")
-
-template.paste(overlay, (0, 0))
+template.paste(Image.open("image1.jpg").resize((713, 536)), (286, 58))
+template.paste(Image.open("image2.jpg").resize((713, 536)), (1015, 58))
+template.paste(Image.open("image3.jpg").resize((713, 536)), (286, 607))
+template.paste(Image.open("image4.jpg").resize((713, 536)), (1015, 607))
 
 # Save it!
 filename = os.path.dirname(os.path.realpath(__file__)) + "/archive/" + datetime.datetime.today().strftime("%Y%m%d-%H%M%S") + ".jpg"
 
 template.save(filename)
+# template.save("output.jpg")
 
 # print it out!
-os.system("lp " + filename)
+os.system("lp -o orientation-requested=5 " + filename)
